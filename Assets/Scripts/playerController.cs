08e31b2ce04c21656aa60644cda6f2525b60cbab
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-09-18 10:55:10 -0500

    Displaying Score and Text
    Add score text and win text

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     private void FixedUpdate()
     {
@@ -27,6 +34,17 @@ public class playerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count>=12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
